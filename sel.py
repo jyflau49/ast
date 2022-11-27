e55c47864c4b09a0db35ffb20755fd7eb1cd5815
root <yufunglau@gmail.com>
2022-11-27 15:18:55 +0000

    screenshot added

--- sel.py
+++ sel.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By # type: ignore
 from selenium.webdriver.chrome.options import Options # type: ignore
 from concurrent.futures import ThreadPoolExecutor, wait # type: ignore
 from dotenv import load_dotenv #type: ignore
+import datetime
 import os
 import time
 
@@ -14,11 +15,16 @@ def run_once():
     options.add_argument('--ignore-ssl-errors=yes')
     options.add_argument('--ignore-certificate-errors')
     options.add_argument('--headless')
+    options.add_argument("--window-size=1920,1080")
     options.add_argument("user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36")
     driver = webdriver.Remote("http://grid:4444/wd/hub",options=options)
     print('webdriver loaded')
     driver.get("https://" + os.getenv('URL'))
     time.sleep(5)
+    date_stamp = str(datetime.datetime.now())
+    date_stamp = date_stamp.replace(" ", "_").replace(":", "_").replace("-", "_").replace(".", "_")
+    file_name = date_stamp + ".png"
+    driver.save_screenshot(file_name)
 
     # element = driver.find_element(By.XPATH, '/html/body/h1').text # XPATH
     # print(driver.page_source.encode("utf-8")) # printing HTTP body content
